f1c0873927e461be3b114ae8e8a6043aad610052
Aditi Naik <aditinaik811@gmail.com>
2025-08-25 20:16:34 +0530

    third commit

---
+++ app/allSlamEntries/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AllSlamEntriesPage() {
+  return (
+    <div>
+      <h1>All Slam Entries</h1>
+      {/* Your logic goes here */}
+    </div>
+  );
+}
