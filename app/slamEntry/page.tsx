5f28613d4b470db8271a6887d28ff43452e64926
Aditi Naik <aditinaik811@gmail.com>
2025-08-25 20:14:19 +0530

    second commit

--- app/slamEntry/page.tsx
+++ app/slamEntry/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { supabase } from "@/lib/SupabaseClient"
-import { getDisplayName } from "next/dist/shared/lib/utils"
+
 import { useState } from "react"
 import { useEffect } from "react"
 export default function SlamEntry() {
